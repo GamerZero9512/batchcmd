5751ca09924150b9c31b221f9de04fd7e087054e
GamerZero9512 <josh.abedelmassieh@gmail.com>
2026-02-06 12:05:36 +0000

    Update cmd.cmd

--- cmd.cmd
+++ cmd.cmd
@@ -1,11 +1,12 @@
 @echo off
-title Command Prompt
 cls
 ver
 :input
+title Command Prompt
 echo.
 set "input="
 set /p "input=%cd%>"
 title Command Prompt - %input%
 call %~dp0cmdactions %input%
 goto :input
+
